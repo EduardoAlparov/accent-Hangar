eaaeddee864e383c957d073518508d47dc105607
EduardoAlparov <workman133@yandex.ru>
2023-11-26 12:26:10 +0500

    26.11.23 commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,6 @@ import './lazyload';
 import detectTouch from './detectTouch';
 import setScrollbarWidth from './setScrollbarWidth';
 import masks from './masks';
-import validation from './validation';
-// import anchorLinks from './anchorLinks';
 import accordions from './accordions';
 import { Modal } from "./Modal";
 import tabs from './tabs';
@@ -28,55 +26,48 @@ import setHeaderPadding from './setHeaderPadding';
 import certificatesSwiper from './certificatesSwiper';
 import articleSwipers from './articleSwipers';
 import pricesTabControl from './pricesTabControl';
-// import checkOverflow from './checkOverflow';
 import setActiveArticle from './setActiveArticle';
 
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener('DOMContentLoaded', function () {
-    detectTouch();
-    setScrollbarWidth();
-    masks();
-    validation();
-    // anchorLinks();
-    accordions();
-    tabs();
-    menu();
-    fancybox();
-    
-    headerSetting();
-    dropMenuControl();
-    mobileMenuOpen();
-    mobileMenuSwiper();
-    selectContol();
-    calculationSwiper();
-    projectsSwiper();
-    reviewsSwiper();
-    inputFileStyling();
-    stagesListSwiper();
-    expandReviewText();
-    productionSwiper();
-    setHeaderPadding();
-    certificatesSwiper();
-    articleSwipers();
-    pricesTabControl();
-    // checkOverflow();
-    setActiveArticle();
+  detectTouch();
+  setScrollbarWidth();
+  masks();
+  validation();
+  // anchorLinks();
+  accordions();
+  tabs();
+  menu();
+  fancybox();
 
-    const modal = new Modal({
-        isOpen: (modal) => {
-        },
-        isClose: (modal) => {
-        },
-    });
-});
+  headerSetting();
+  dropMenuControl();
+  mobileMenuOpen();
+  mobileMenuSwiper();
+  selectContol();
+  calculationSwiper();
+  projectsSwiper();
+  reviewsSwiper();
+  inputFileStyling();
+  stagesListSwiper();
+  expandReviewText();
+  productionSwiper();
+  setHeaderPadding();
+  certificatesSwiper();
+  articleSwipers();
+  pricesTabControl();
+  // checkOverflow();
+  setActiveArticle();
 
-document.addEventListener('lazyloaded', () => {
-    ScrollTrigger.refresh();
+  const modal = new Modal({
+    isOpen: (modal) => {
+    },
+    isClose: (modal) => {
+    },
+  });
 });
 
 window.addEventListener('load', function () {
-    document.body.classList.add('loaded');
-    ScrollTrigger.refresh();
-    setTimeout(() => document.body.classList.add('animatable'), 300);
+  document.body.classList.add('loaded');
 });
